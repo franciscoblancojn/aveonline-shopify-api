eb632f0e2b603168e8040fe3ec4df314811e4c83
franciscoblancojn <blancofrancisco34@gmail.com>
2021-07-16 10:27:50 -0500

    init index controllers and middlewares

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,11 @@
 require('module-alias/register')
+const middlewares = require('@middlewares/index')
+const controllers = require('@controllers/index')
 
 const router = require('express').Router()
+
+
+router.get("/",[middlewares.index],controllers.index)
 
 
 module.exports = router
