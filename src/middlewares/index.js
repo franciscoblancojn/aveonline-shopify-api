a96c89a8e60d6947d9cb81f658de828d6c894536
franciscoblancojn <blancofrancisco34@gmail.com>
2021-07-16 10:37:06 -0500

    send page

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,3 +1,44 @@
+require('module-alias/register')
+const fs = require('fs');
+
 exports.index = async (req, res, next) => {
-    res.send("hola")
+    const {shop , session } = req.query;
+    if(session){
+        fs.readFile(__dirname + '/../page/app.html', (err, html) => {
+            if (err) {
+                res.status(400).send({
+                    type:"error",
+                    error:`${err}`
+                })
+                return
+            }    
+            res.send(`
+                <script>
+                    appJson = ${JSON.stringify(req.query)};
+                </script>
+                ${html}
+            `)   
+            return
+        });
+    }else if (shop) {
+
+        // const state = nonce();
+        // // shopify callback redirect
+        // const redirectURL = process.env.SHOPIFY_APP_URL + '/auth/callback';
+
+        // // Install URL for app install
+        // const shopifyURL = 'https://' + shop +
+        //     '/admin/oauth/request_grant?client_id=' + process.env.SHOPIFY_API_KEY +
+        //     '&scope=' + process.env.SHOPIFY_API_SCOPES +
+        //     '&redirect_uri=' + redirectURL +
+        //     '&state=' + state ;
+
+        // res.cookie('state', state);
+        // res.redirect(shopifyURL);
+    } else {
+        return res.status(400).send({
+            type:"error",
+            error:'Missing "Shop Name" parameter!!'
+        });
+    }
 }
