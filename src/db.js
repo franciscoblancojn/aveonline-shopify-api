88050d61451bb7b6564424775f991b5efdd056a0
franciscoblancojn <blancofrancisco34@gmail.com>
2021-07-16 11:16:18 -0500

    db

---
+++ src/db.js
@@ -0,0 +1,12 @@
+const dbMonk = require('monk')('localhost/db');
+
+exports.get = async ({collection,query}) => {
+    var db = dbMonk.get(collection)
+    var respond = await db.find(query)
+    return respond
+}
+exports.post = async ({collection,data}) => {
+    var db = dbMonk.get(collection)
+    var respond = await db.insert(data)
+    return respond
+}
