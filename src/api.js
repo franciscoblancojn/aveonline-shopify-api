90f43eb1b503766a05b9e54cf085dd1e1bade362
franciscoblancojn <blancofrancisco34@gmail.com>
2021-07-29 16:59:35 -0500

    Access-Control-Request

--- src/api.js
+++ src/api.js
@@ -23,6 +23,8 @@ app.use(bodyParser.raw())
 app.use(express.urlencoded({ extended: false }))
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Request-Headers', '*')
+  res.header('Access-Control-Request-Methods', '*')
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
