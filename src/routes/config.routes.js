2fe065472dcdf234e559ba0bc214ffa9bfbd3589
franciscoblancojn <blancofrancisco34@gmail.com>
2021-10-19 16:16:08 -0500

    /config middlewares

--- src/routes/config.routes.js
+++ src/routes/config.routes.js
@@ -4,11 +4,53 @@ require('module-alias/register')
 const config = require('@controllers/config/_index')
 
 const router = require('express').Router()
+const fmiddlewares = require('fmiddlewares')
 
 router.post(
     "/",
     [
-
+        fmiddlewares.validateItem({
+            "eneable": {
+                type:"boolean"
+            },
+            "user": {
+                type:"string"
+            },
+            "password": {
+                type:"string"
+            },
+            "dsnitre": {
+                type:"string"
+            },
+            "dsdirre": {
+                type:"string"
+            },
+            "dstelre": {
+                type:"string"
+            },
+            "dscelularre": {
+                type:"string"
+            },
+            "dscorreopre": {
+                type:"string"
+            },
+            "cuenta": {
+                type:"string"
+            },
+            "option_cuenta": {
+                type:"array"
+            },
+            "agente": {
+                type:"string"
+            },
+            "option_agente": {
+                type:"array"
+            },
+            "valorMinimo": {
+                type:"boolean"
+            },
+        
+        })
     ],
     config.save
 )
