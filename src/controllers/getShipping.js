ec5b0951cf69594501d3ac06ac39894b1010953d
franciscoblancojn <blancofrancisco34@gmail.com>
2021-10-19 15:59:26 -0500

    config in database

--- src/controllers/getShipping.js
+++ src/controllers/getShipping.js
@@ -1,10 +1,16 @@
+//require libs
+require('module-alias/register')
+
+//require db
+const db = require('@app/db')
+
 const getShipping = (req,res) => {
     console.log("[POST] /shipping");
     const {shop} = req.query
     res.send({
         "rates": [
             {
-                "service_name": "Aveonline"+shop,
+                "service_name": "Aveonline",
                 "service_code": "Aveonline",
                 "total_price": "1000000",
                 "description": "This is the fastest option by far",
