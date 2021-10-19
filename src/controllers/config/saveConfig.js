505ada014633ccbfb7837048417636d37a9e8f24
franciscoblancojn <blancofrancisco34@gmail.com>
2021-10-19 16:51:26 -0500

    saveConfig

--- src/controllers/config/saveConfig.js
+++ src/controllers/config/saveConfig.js
@@ -1,4 +1,28 @@
-const saveConfig = (req,res) => {
+require("module-alias/register");
+const db = require("@app/db");
 
+const saveConfig = async (req,res) => {
+    try {
+        const result = await db.put({
+            where: req.query,
+            data: {
+                $set: {
+                    ...req.body,
+                },
+            },
+            table: `shops`,
+        });
+        res.send({
+            type:"ok",
+            msj:"Config Save",
+            result
+        })
+    } catch (error) {
+        res.status(500).send({
+            type:"error",
+            msj:`${error}`,
+            error
+        })
+    }
 }
 module.exports = saveConfig
