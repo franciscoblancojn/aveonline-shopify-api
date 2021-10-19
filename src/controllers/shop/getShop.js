c7dbbbfe4a97d77c7a43b74b8988801240a6413d
franciscoblancojn <blancofrancisco34@gmail.com>
2021-10-19 16:49:48 -0500

    getShop

--- src/controllers/shop/getShop.js
+++ src/controllers/shop/getShop.js
@@ -2,6 +2,21 @@ require("module-alias/register");
 const db = require("@app/db");
 
 const getShop = async (req,res) => {
-
+    try {
+        const result = await db.get({
+            query:req.query,
+            table:"shops"
+        })
+        res.send({
+            type:"ok",
+            result
+        })
+    } catch (error) {
+        res.status(500).send({
+            type:"error",
+            msj:`${error}`,
+            error
+        })
+    }
 }
 module.exports = getShop
