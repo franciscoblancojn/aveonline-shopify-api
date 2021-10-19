2b6a7ffe3b11d977e68de344b7cc293471883033
franciscoblancojn <blancofrancisco34@gmail.com>
2021-10-19 17:09:32 -0500

    validate shop in getShipping

--- src/controllers/shipping/getShipping.js
+++ src/controllers/shipping/getShipping.js
@@ -1,22 +1,33 @@
-//require libs
-require('module-alias/register')
+require("module-alias/register");
+const db = require("@app/db");
 
-//require db
-const db = require('@app/db')
-
-const getShipping = (req,res) => {
-    console.log("[POST] /shipping");
-    const {shop} = req.query
-    res.send({
-        "rates": [
-            {
-                "service_name": "Aveonline",
-                "service_code": "Aveonline",
-                "total_price": "1000000",
-                "description": "This is the fastest option by far",
-                "currency": "COP"
-            }
-        ]
-    })
+const getShipping = async (req,res) => {
+   try {
+        const result = await db.get({
+            query:req.query,
+            table:"shops"
+        })
+        const shop = result[0]
+        if(!shop){
+            throw new Error("Invalid Shop")
+        }
+        res.send({
+            "rates": [
+                {
+                    "service_name": "Aveonline",
+                    "service_code": "Aveonline",
+                    "total_price": "1000000",
+                    "description": "This is the fastest option by far",
+                    "currency": "COP"
+                }
+            ]
+        })
+   } catch (error) {
+        res.status(500).send({
+            type:"error",
+            msj:`${error}`,
+            error
+        })
+   }
 }
 module.exports = getShipping
