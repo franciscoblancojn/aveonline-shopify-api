75382fae5572d716227b1159a7288e343eae37c4
franciscoblancojn <blancofrancisco34@gmail.com>
2021-10-19 15:55:46 -0500

    testing shop

--- src/controllers/getShipping.js
+++ src/controllers/getShipping.js
@@ -1,10 +1,11 @@
 const getShipping = (req,res) => {
     console.log("[POST] /shipping");
+    const {shop} = req.query
     res.send({
         "rates": [
             {
-                "service_name": "Aveonlineapp",
-                "service_code": "Aveonlineapp",
+                "service_name": "Aveonline"+shop,
+                "service_code": "Aveonline",
                 "total_price": "1000000",
                 "description": "This is the fastest option by far",
                 "currency": "COP"
