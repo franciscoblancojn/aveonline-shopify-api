414ec9aebcebb2dda58af33fc2f254b4b83a98d1
franciscoblancojn <blancofrancisco34@gmail.com>
2021-10-20 11:11:13 -0500

    postShipping

--- src/shopify/shipping/_index.js
+++ src/shopify/shipping/_index.js
@@ -1,5 +1,7 @@
 require('module-alias/register')
 
 module.exports = { 
-    get :  require("@shopify/shipping/get")
+    get :  require("@shopify/shipping/get"),
+    post :  require("@shopify/shipping/post"),
+    delete :  require("@shopify/shipping/delete"),
 }
