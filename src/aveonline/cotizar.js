ed13d0b401cd674898b1b1f5b653e201db45c093
franciscoblancojn <blancofrancisco34@gmail.com>
2021-10-20 14:35:00 -0500

    data, products in cotizar

--- src/aveonline/cotizar.js
+++ src/aveonline/cotizar.js
@@ -3,7 +3,22 @@ const request = require("@functions/request");
 const getToken = require("@aveonline/getToken");
 
 const cotizar = async ({config,checkout}) => {
+    if(checkout.currency !== "COP" || checkout.destination.country !== "CO"){
+        return null
+    }
     console.log(checkout);
+    var valorrecaudo = 0
+    const products =  checkout.items.map((e)=>{
+        valorrecaudo += e.quantity * e.price
+        return {
+            "alto":1,//pendiente
+            "largo":1,//pendiente
+            "ancho":1,//pendiente
+            "peso": e.grams / 1000,
+            "unidades": e.quantity, 
+            "valorDeclarado": e.price,//pendiente  valor declarado custom
+        }
+    })
     const data = {
         "tipo"          : "cotizarDoble",
         "access"        : "",
@@ -13,8 +28,8 @@ const cotizar = async ({config,checkout}) => {
         "destino"       : "MEDELLIN(ANTIOQUIA)",//pendiente
         "idasumecosto"  : 1,
         "contraentrega" : 1,
-        "valorrecaudo"  : "valorrecaudo",//pendiente
-        "productos"     : "productos",//pendiente
+        "valorrecaudo"  : valorrecaudo,
+        "productos"     : products,//pendiente
         "valorMinimo"   : (config.valorMinimo)?1:0
     }
     return data
