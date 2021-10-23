f723aaefdad01b3e5072662908eab2668d9ec6f7
franciscoblancojn <blancofrancisco34@gmail.com>
2021-10-22 21:00:50 -0500

    init controller Script

--- src/controllers/scripts/addScript.js
+++ src/controllers/scripts/addScript.js
@@ -1,7 +1,15 @@
 require("module-alias/register");
 const {scripts} = require("@shopify/_index");
 
-const addScript = (req,res) => {
-
+const addScript = async (req,res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).send({
+            type:"error",
+            msj:`${error}`,
+            error
+        })
+    }
 }
 module.exports = addScript
