5acdaacb723a3e4b1c20bcf7bc2446821b13a105
franciscoblancojn <blancofrancisco34@gmail.com>
2021-10-25 08:13:33 -0500

    testing addScripts

--- src/controllers/scripts/addScript.js
+++ src/controllers/scripts/addScript.js
@@ -3,7 +3,10 @@ const {scripts} = require("@shopify/_index");
 
 const addScript = async (req,res) => {
     try {
-        
+        const result = await scripts.get()
+        res.send({
+            result
+        })
     } catch (error) {
         res.status(500).send({
             type:"error",
