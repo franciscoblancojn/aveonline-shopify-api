03d7453994b23f2e205b5529980ccab69c837214
franciscoblancojn <blancofrancisco34@gmail.com>
2021-12-08 12:30:37 -0500

    console in log

--- src/functions/log.js
+++ src/functions/log.js
@@ -4,9 +4,14 @@ const db = require("@app/db");
 
 module.exports = async ({ type = "", data = {} }) => {
     if (env.LOG === "TRUE") {
+        console.log({
+            type,
+            ...data
+        });
         await db.post({
             data: {
                 type,
+                date:(new Date()),
                 ...data,
             },
             table: "logs",
