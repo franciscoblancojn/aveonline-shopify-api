c6f247e2263f83bb9c69637415f3bad9cf73bccc
franciscoblancojn <blancofrancisco34@gmail.com>
2021-12-08 12:33:18 -0500

    log in token

--- src/controllers/tokens/saveToken.js
+++ src/controllers/tokens/saveToken.js
@@ -1,6 +1,7 @@
 require("module-alias/register");
 const env = require("@app/env");
 const db = require("@app/db");
+const log = require("@app/functions/log");
 
 const saveToken = async (req, res) => {
     try {
@@ -23,13 +24,10 @@ const saveToken = async (req, res) => {
             result
         })
     } catch (error) {
-        if (env.LOG === "TRUE") {
-            console.log(error);
-            await db.post({
-                data: error,
-                table: "logs",
-            });
-        }
+        await log({
+            type: "error",
+            data: error,
+        });
         res.status(500).send({
             type:"error",
             msj:`${error}`,
