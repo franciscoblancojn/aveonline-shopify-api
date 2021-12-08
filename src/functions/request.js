28c6607b7223e8b913b643b584e0b2471f2ff051
franciscoblancojn <blancofrancisco34@gmail.com>
2021-12-08 12:29:40 -0500

    log in request

--- src/functions/request.js
+++ src/functions/request.js
@@ -2,37 +2,28 @@ require("module-alias/register");
 const axios = require("axios");
 const db = require("@app/db");
 const env = require("@app/env");
+const log = require("@app/functions/log");
 
 module.exports = async (config) => {
     try {
         const response = await axios(config);
         const data = response.data;
-        if (env.LOG === "TRUE") {
-            console.log({
+        await log({
+            type: "request",
+            data: {
                 send: config,
                 result: data,
-            });
-            await db.post({
-                data: {
-                    send: config,
-                    result: data,
-                    date:(new Date())
-                },
-                table: "logs",
-            });
-        }
+            },
+        });
         return {
             type: "ok",
             ...data,
         };
     } catch (error) {
-        if (env.LOG === "TRUE") {
-            console.log(error);
-            await db.post({
-                data: error,
-                table: "logs",
-            });
-        }
+        await log({
+            type: "error",
+            data: error,
+        });
         return {
             type: "error",
             msj: `${error}`,
