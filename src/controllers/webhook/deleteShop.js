650eeb4b77948a1ff920e98c38faf420c2f0e35b
franciscoblancojn <blancofrancisco34@gmail.com>
2022-03-14 10:46:05 -0500

    validate accessToken

--- src/controllers/webhook/deleteShop.js
+++ src/controllers/webhook/deleteShop.js
@@ -3,30 +3,55 @@ const env = require("@app/env");
 const db = require("@app/db");
 const log = require("@app/functions/log");
 
-const deleteShop = async (req,res) => {
+const deleteShop = async (req, res) => {
     try {
         await log({
             type: "deleteShop",
             data: {
-                method : req.method,
+                method: req.method,
                 header: req.header,
                 query: req.query,
-                body:req.body,
+                body: req.body,
             },
         });
+
+        const { accessToken } = req.body;
+
+        if (!accessToken) {
+            res.status(401).send({
+                type: "error",
+                msj: "accessToken is required",
+            });
+        }
+        const result = await db.get({
+            query: req.query,
+            table: "shops",
+            query: {
+                token: accessToken,
+            },
+        });
+        const shop = result[0];
+
+        if (!shop) {
+            res.status(401).send({
+                type: "error",
+                msj: "accessToken is invalid",
+            });
+        }
+
         return res.send({
-            type:"ok",
-        })
+            type: "ok",
+        });
     } catch (error) {
         await log({
             type: "error",
             data: error,
         });
         res.status(500).send({
-            type:"error",
-            msj:`${error}`,
-            error
-        })
+            type: "error",
+            msj: `${error}`,
+            error,
+        });
     }
-}
-module.exports = deleteShop
+};
+module.exports = deleteShop;
