30eb16bc1e11e39132dd3ba120d713d27850d00c
franciscoblancojn <blancofrancisco34@gmail.com>
2022-03-14 08:26:31 -0500

    init webhook

---
+++ src/controllers/webhook/deleteShop.js
@@ -0,0 +1,31 @@
+require("module-alias/register");
+const env = require("@app/env");
+const db = require("@app/db");
+const log = require("@app/functions/log");
+
+const deleteShop = async (req,res) => {
+    try {
+        await log({
+            type: "deleteShop",
+            data: {
+                header: req.header,
+                query: req.query,
+                body:req.body,
+            },
+        });
+        return res.send({
+            type:"ok",
+        })
+    } catch (error) {
+        await log({
+            type: "error",
+            data: error,
+        });
+        res.status(500).send({
+            type:"error",
+            msj:`${error}`,
+            error
+        })
+    }
+}
+module.exports = deleteShop
