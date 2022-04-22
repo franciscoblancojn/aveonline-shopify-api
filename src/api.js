34bb72e9564c596abfddc3f8da9e7262900dd820
franciscoblancojn <blancofrancisco34@gmail.com>
2022-04-22 13:09:51 -0500

    404

--- src/api.js
+++ src/api.js
@@ -1,53 +1,69 @@
 // requires
-require('module-alias/register')
-const env = require("@app/env")
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const routes = require('@routes/_index.routes')
-const cors = require('cors')
+require("module-alias/register");
+const env = require("@app/env");
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const routes = require("@routes/_index.routes");
+const cors = require("cors");
 
 /**
  * @description port and rute
  */
-const port = env.PORT || 3001
+const port = env.PORT || 3001;
 
 /**
  * @description load app
  */
-const app = express()
-app.set('port', port)
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(bodyParser.raw())
-app.use(express.urlencoded({ extended: false }))
+const app = express();
+app.set("port", port);
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-  res.header('Content-Security-Policy', "frame-ancestors '*'")
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Request-Headers', '*')
-  res.header('Access-Control-Request-Methods', '*')
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
-  next()
+    res.header("Content-Security-Policy", "frame-ancestors '*'");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Request-Headers", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
 });
-app.use(cors({
-  origin: '*'
-}))
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 
-app.use('/api/v1', routes)
+app.use("/api/v1", routes);
 
 /**
  * use folder page for rutes not enpoints
  */
-app.use('/api/v1/public',express.static(path.join(__dirname, 'page')))
+app.use("/api/v1/public", express.static(path.join(__dirname, "page")));
 
+app.use("*", (req, res) => {
+    res.status(404).send({
+        status: 404,
+        code: 404,
+        msg: "this endpoint doesn't exist",
+        type: "error",
+    });
+});
 /**
  * app.listen
  * @description enpoint listen
  */
 app.listen(port, function () {
-  console.info(`[INFO]:: Server running on port ${port}`)
-})
+    console.info(`[INFO]:: Server running on port ${port}`);
+});
